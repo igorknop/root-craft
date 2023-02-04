4d9fb9d9a92f2451ed2276b653d867ec4d405f18
Igor Knop <igorknop@gmail.com>
2023-02-04 00:46:46 -0300

    :sparkles: add a item card

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
 import { useState } from "react";
 import "./App.css";
+import ItemCard from "./components/ItemCard";
 import PlaceCard from "./components/PlaceCard";
 import TimeTrack from "./components/TimeTrack";
 
 function App() {
   return (
     <div className="App">
-      <h1>Root Craft</h1>
-      <TimeTrack time={2} />
-      <h2>Places</h2>
-      <PlaceCard
-        place={{
-          name: "The Forest",
-          description: "A dark and foreboding forest.",
+      <section>
+        <h1>Root Craft</h1>
+        <TimeTrack time={2} />
+      </section>
+      <section>
+        <h2>Places</h2>
+        <PlaceCard
+          place={{
+            name: "The Forest",
+            description: "A dark and foreboding forest.",
+            tokens: [
+              {
+                id: "P1",
+              },
+            ],
+          }}
+        />
+      </section>
+      <section>
+      <h2>Items</h2>
+      <ItemCard
+        item={{
+          name: "Wood",
+          description: "A piece of wood.",
           tokens: [
             {
-              id: "P1",
+              id: "HL",
             },
           ],
         }}
-      />
+        />
+
+      </section>
     </div>
   );
 }
---
+++ src/components/ItemCard.tsx
@@ -0,0 +1,27 @@
+import Token from '../Token';
+import styles from './ItemCard.module.css';
+import TokenElement from './TokenElement';
+
+
+interface ItemCardProps {
+  item: {
+    name: string;
+    description: string;
+    tokens: Token[];
+  };
+}
+
+
+export default function ItemCard({ item }: ItemCardProps) {
+  return (
+    <div className={styles.ItemCard}>
+      <h2>{item.name}</h2>
+      <p>{item.description}</p>
+      <div className={styles.tokens}>
+        {item.tokens.map((token) => (
+          <TokenElement token={token} key={token.id}/>
+        ))}
+      </div>
+    </div>
+  );
+}
