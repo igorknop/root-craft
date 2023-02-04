881390231ca0a70e713cb42f5fbe7efb2072d14c
Igor Knop <igorknop@gmail.com>
2023-02-04 00:00:09 -0300

    :sparkles: add a time track

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import TimeTrack from './components/TimeTrack'
 
 function App() {
 
   return (
     <div className="App">
       <h1>Root Craft</h1>
+      <TimeTrack time={2}/>
     </div>
   )
 }
---
+++ src/components/TimeTrack.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import styles from "./TimeTrack.module.css";
+
+const MAX_TIME = 12;
+const TIME_SLOTS = [
+  { time: 1, label: "Dawn" },
+  { time: 2, label: "Morning" },
+  { time: 3, label: "Noon" },
+  { time: 4, label: "Afternoon" },
+  { time: 5, label: "Dusk" },
+  { time: 6, label: "Evening" },
+  { time: 7, label: "Midnight" },
+  { time: 8, label: "Night" },
+];
+
+interface TimeTrackProps {
+  time: number;
+}
+
+export default function TimeTrack({ time }: TimeTrackProps) {
+  return (
+    <div className={styles.TimeTrack}>
+      <h2>Time Track</h2>
+      <ol className={styles.TimeTrackSlots}>
+        {TIME_SLOTS.map((slot) => (
+          <li className={time === slot.time ? styles.selectedSlot: ""} key={`slot${slot.time}`}>
+            <div>{slot.label}</div>
+            <div>{time === slot.time ? "X" : ""}</div>
+          </li>
+        ))}
+    </ol>
+      </div>
+  );
+}
